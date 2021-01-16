2cd581ec4dbf8ea65ef8b031bb0308f4c1095961
Shannon Heh <shannon.heh@princeton.edu>
2021-01-16 11:39:51 -0800

    firebase setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/database";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBH52fYwnC9Y6HfGj4CVMDVlwdzBS285R0",
+    authDomain: "cruzhacks-2021-sn.firebaseapp.com",
+    projectId: "cruzhacks-2021-sn",
+    storageBucket: "cruzhacks-2021-sn.appspot.com",
+    messagingSenderId: "323432439022",
+    appId: "1:323432439022:web:1e803cccf566d37ed567ff",
+    measurementId: "G-D6M9RMBSDW"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
